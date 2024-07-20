b77f5efb1d3c108c6942e944f19dd5523f0a454b
Pawan Kushwah <pawankushwahmail@gmail.com>
2024-07-20 20:48:40 +0530

    Added CORS in the Express App
    Now any body can access the resources (Public Files)

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
 const express = require("express");
 const app = express();
+
 const port = process.env.PORT || 9001;
-require("dotenv").config();
 
-const path = require("path");
-
+app.use(cors({
+    origin: "*",
+}));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
