0ba3e72796710cee081832fa9a4889df0c382648
Pawan Kushwah <pawankushwahmail@gmail.com>
2024-08-20 00:22:00 +0530

    Revert "removing extra part of CORS fix"
    This reverts commit c35115084678b2dce883e363e4d20ea8d64e16f0.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const app = express();
-const corsVercel = require("allowCors");
 
 const port = process.env.PORT || 9001;
 
